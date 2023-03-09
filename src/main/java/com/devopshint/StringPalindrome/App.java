63b1d8bd421035c0acc2f4eeb0d1163c0728ea94
DevOpsHint <83763090+devopshint@users.noreply.github.com>
2023-03-09 13:27:34 +0530

    Update App.java

--- src/main/java/com/devopshint/StringPalindrome/App.java
+++ src/main/java/com/devopshint/StringPalindrome/App.java
@@ -1,25 +1,12 @@
 package com.devopshint.StringPalindrome;
-public class App {
+import org.joda.time.LocalTime;
 
-	public boolean isPalindrome(String input) {
-
-		if (input == null) {
-			throw new IllegalArgumentException("input shouldn't be null");
-		}
-
-		if (input.equals(reverse(input))) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	private String reverse(String input) {
-		String rev = "";
-		for (int i = input.length() - 1; i >= 0; i--) {
-			rev = rev + input.charAt(i);
-		}
-		return rev;
-	}
-
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
 }
